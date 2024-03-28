cfec3d05bad63eae1bab9d671dfd0a2e169167a8
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-03-28 08:59:26 +0530

    Check if start and end char are same before starting the palindrome validation

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -2,13 +2,19 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         total = len(s)
         result = ""
+        max_len = 0
 
         for start in range(total):
             for end in range(total, -1, -1):
+                
+                if s[start] != s[end-1]:
+                    continue
                 p = s[start:end]
                 if p == p[::-1]:
-                    if len(p) > len(result):
+                    len_substring = end - start + 1
+                    if len_substring > max_len:
                         result = p
+                        max_len = len_substring
                     break
 
         return result
