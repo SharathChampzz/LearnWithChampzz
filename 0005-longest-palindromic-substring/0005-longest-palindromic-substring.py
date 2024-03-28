1a3110b1ac4ba1c2c3d3c744cfe7de5ee04ad010
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-03-28 20:52:10 +0530

    Update 0005-longest-palindromic-substring.py

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -2,19 +2,18 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         total = len(s)
         result = ""
-        max_len = 0
+        max_length = 0  # Variable to store the length of the longest palindrome found
 
         for start in range(total):
-            for end in range(total, -1, -1):
-                
+            # Optimize end range to start from the maximum possible length of palindrome
+            for end in range(total, start + max_length, -1):
+                # Check if the end characters are same, else there is no need to proceed further
                 if s[start] != s[end-1]:
                     continue
-                p = s[start:end]
-                if p == p[::-1]:
-                    len_substring = end - start + 1
-                    if len_substring > max_len:
-                        result = p
-                        max_len = len_substring
+                # Check if the substring is a palindrome and longer than the current result
+                if s[start:end] == s[start:end][::-1] and end - start > max_length:
+                    result = s[start:end]
+                    max_length = end - start  # Update max_length with the new longest palindrome length
                     break
 
         return result
