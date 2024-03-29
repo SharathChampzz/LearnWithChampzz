33022be3befffaa96bcd0164977b568736eb8378
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-03-29 15:51:36 +0530

    Time: 35 ms (67.88%), Space: 16.7 MB (17.93%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -3,11 +3,18 @@ class Solution:
         negative = x*-1 > 0
 
         if negative:
-            x *= -1
+            x *= -1 # make it as positive
 
-        reverse = int(str(x)[::-1])
+        # reverse = int(str(x)[::-1])
+        # result = -reverse if negative else reverse
+        
+        reverse = 0
+        while x:
+            reverse = reverse * 10 + x % 10
+            x = x // 10
+            
         result = -reverse if negative else reverse
-
+        
         if result >= pow(2,31)-1 or result <= pow(-2, 31):
             return 0
 
