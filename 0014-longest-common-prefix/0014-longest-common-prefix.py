8bcf72fdab56861f62ac1b737f3748999372cddc
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-04-02 20:16:23 +0530

    Time: 31 ms (91.84%), Space: 16.6 MB (90.34%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,22 +1,45 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        length = len(strs)
-        prefix_str = ""
+        if not strs:
+            return ""
 
-        # get smallest string
-        small_str = strs[0] # assume first word is smallest
-        small_str_len = len(small_str)
-        for word in strs[1:]:
-            temp_length = len(word)
-            if temp_length < small_str_len:
-                small_str = word
-                small_str_len = temp_length
+        # Find the shortest string
+        min_len = min(len(s) for s in strs)
+        low, high = 0, min_len - 1
 
-        # check if prefix exists in all the strings 
-        for char_index, char in enumerate(small_str):
-            for i in range(length):
-                if char != strs[i][char_index]:
-                    return prefix_str
-            prefix_str += char
+        while low <= high:
+            mid = (low + high) // 2
+            if self.isCommonPrefix(strs, mid):
+                low = mid + 1
+            else:
+                high = mid - 1
 
-        return prefix_str
+        return strs[0][:low]
+
+    def isCommonPrefix(self, strs: List[str], mid: int) -> bool:
+        prefix = strs[0][:mid + 1]
+        return all(s.startswith(prefix) for s in strs)
+
+
+        # start_index = 0
+        # length = len(strs)
+        # prefix_str = ""
+
+        # # get smallest string
+        # small_str = strs[0] # assume first word is smallest
+        # small_str_len = len(small_str)
+        # for word in strs[1:]:
+        #     temp_length = len(word)
+        #     if temp_length < small_str_len:
+        #         small_str = word
+        #         small_str_len = temp_length
+
+        # # check if prefix exists in all the strings 
+        # for char_index, char in enumerate(small_str):
+        #     for i in range(length):
+        #         if char != strs[i][char_index]:
+        #             return prefix_str
+        #     else:
+        #         prefix_str += char
+
+        # return prefix_str
