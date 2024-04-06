f15f03f46a1b8e1598c3b6636c30c6c775be9e1c
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-04-06 10:15:32 +0530

    Time: 38 ms (37.65%), Space: 16.4 MB (89.10%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,23 +1,49 @@
-# from typing import List
-
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
 
-        digit_to_letters = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
-        chars = [digit_to_letters[digit] for digit in digits]
+        # Mapping of digits to letters
+        phone_mapping = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
 
-        def get_combination(los: List[str]) -> List[str]:
-            result = []
+        def generate_combinations(index, current):
+            if index == len(digits):
+                combinations.append(current)
+                return
 
-            if len(los) == 1:
-                return list(los[0])
-            else:
-                temp = get_combination(los[1:])
-                for char in los[0]:
-                    result.extend([char + x for x in temp])
-                return result
+            for letter in phone_mapping[digits[index]]:
+                generate_combinations(index + 1, current + letter)
 
-        final_combination = get_combination(chars)
-        return final_combination
+        combinations = []
+        generate_combinations(0, '')
+        return combinations
+
+
+        # if not digits:
+        #     return []
+
+        # digit_to_letters = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        # chars = [digit_to_letters[digit] for digit in digits]
+
+        # def get_combination(los: List[str]) -> List[str]:
+        #     result = []
+
+        #     if len(los) == 1:
+        #         return list(los[0])
+        #     else:
+        #         temp = get_combination(los[1:])
+        #         for char in los[0]:
+        #             result.extend([char + x for x in temp])
+        #         return result
+
+        # final_combination = get_combination(chars)
+        # return final_combination
