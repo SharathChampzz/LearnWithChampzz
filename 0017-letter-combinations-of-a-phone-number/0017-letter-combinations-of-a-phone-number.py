cd97ffcdc0204000c198eebfa0f167af3c63d47c
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-04-06 10:19:20 +0530

    Time: 28 ms (92.22%), Space: 16.6 MB (9.98%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,31 +1,28 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return []
 
-        # Mapping of digits to letters
-        phone_mapping = {
-            '2': 'abc',
-            '3': 'def',
-            '4': 'ghi',
-            '5': 'jkl',
-            '6': 'mno',
-            '7': 'pqrs',
-            '8': 'tuv',
-            '9': 'wxyz'
-        }
+        res = []
+        digitToChar = {"2":"abc",
+        "3":"def",
+        "4":"ghi",
+        "5":"jkl",
+        "6":"mno",
+        "7":"pqrs",
+        "8":"tuv",
+        "9":"wxyz"        }
 
-        def generate_combinations(index, current):
-            if index == len(digits):
-                combinations.append(current)
+        def backtrack(i,curStr):
+            if len(curStr)==len(digits):
+                res.append(curStr)
                 return
+            for c in digitToChar[digits[i]]:
+                backtrack(i+1,curStr+c)
 
-            for letter in phone_mapping[digits[index]]:
-                generate_combinations(index + 1, current + letter)
+        if digits:
+            backtrack(0,"")
 
-        combinations = []
-        generate_combinations(0, '')
-        return combinations
+        return res
+        
 
 
         # if not digits:
