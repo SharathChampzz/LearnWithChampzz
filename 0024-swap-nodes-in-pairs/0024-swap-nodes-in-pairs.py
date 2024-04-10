7eb1fe621065434e6a63bbbe79eac65e42bb3c4e
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-04-10 11:33:43 +0530

    Time: 34 ms (66.86%), Space: 16.6 MB (6.92%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -8,7 +8,8 @@ class Solution:
         dummy = ListNode()
         dummy.next = head
         current = dummy
-
+        
+        # there should be atleast 2 more nodes for swapping
         while current.next and current.next.next:
             # read the 3 nodes
             node1, node2, node3 = current.next, current.next.next, current.next.next.next
@@ -16,6 +17,7 @@ class Solution:
             # swap the nodes
             current.next, node2.next, node1.next = node2, node1, node3
 
+            # move the current node to the next pair
             current = node1
         
         return dummy.next
