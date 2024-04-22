c123fad2822c86d83307b26d832ad3dfe6c7452a
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-04-22 18:13:00 +0530

    Time: 34 ms (60.24%), Space: 16.5 MB (60.92%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -3,14 +3,20 @@ class Solution:
         mapping = {}
         words = s.split()
 
+        # there is no full match
         if len(pattern) != len(words):
             return False
         
         for index, char in enumerate(pattern):
+            
+            # if pattern encountered for the first time, add the mapping
             if char not in mapping:
+                
+                # check if pattern is already mapped to some other word
                 if words[index] in mapping.values():
                     return False
                 mapping[char] = words[index]
+                
             else:
                 if mapping[char] != words[index]:
                     return False
