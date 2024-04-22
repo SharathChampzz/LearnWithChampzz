c8c027e359682442407501e2d99b233aac26e665
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-04-22 17:58:16 +0530

    Time: 38 ms (30.62%), Space: 16.8 MB (14.34%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,19 +1,20 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        word_to_char = {}
-        char_to_word = {}
+        mapping = {}
         words = s.split()
 
         if len(pattern) != len(words):
             return False
         
-        for char, word in zip(pattern, words):
-            if char not in char_to_word and word not in word_to_char:
-                char_to_word[char] = word
-                word_to_char[word] = char
-            elif char in char_to_word and char_to_word[char] != word:
-                return False
-            elif word in word_to_char and word_to_char[word] != char:
-                return False
+        for index, char in enumerate(pattern):
+            if char not in mapping:
+                if words[index] in mapping.values():
+                    return False
+                mapping[char] = words[index]
+            else:
+                if mapping[char] != words[index]:
+                    return False
 
         return True
+                
+        
