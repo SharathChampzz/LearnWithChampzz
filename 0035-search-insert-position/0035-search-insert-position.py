e5d2c6529314d8834cf4b7261a06e01932576b61
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-04-23 18:29:58 +0530

    Time: 54 ms (25.50%), Space: 17.4 MB (49.55%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -8,11 +8,11 @@ class Solution:
             mid = left + (right-left)//2
 
             if nums[mid] == target:
-                return mid
+                return mid # if it is found return that index
 
             if target < nums[mid]:
                 right = mid - 1
             else:
                 left = mid + 1
 
-        return left
+        return left # left index will hold the next possible insertion for the given target
