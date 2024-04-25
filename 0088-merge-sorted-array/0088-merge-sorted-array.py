df8e4cfb03fb9fd412098b1c5c6bb84772cbb0f7
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-04-25 09:09:33 +0530

    Time: 45 ms (21.07%), Space: 16.5 MB (78.05%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,12 +1,30 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        index1 = m - 1
+        index2 = n - 1
+        merged_index = m + n - 1
+
+        while index1 >= 0 and index2 >= 0:
+            if nums1[index1] > nums2[index2]:
+                nums1[merged_index] = nums1[index1]
+                index1 -= 1
+            else:
+                nums1[merged_index] = nums2[index2]
+                index2 -= 1
+            merged_index -= 1
+
+        while index2 >= 0:
+            nums1[merged_index] = nums2[index2]
+            index2 -= 1
+            merged_index -= 1
+
+    def Tempmerge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
         nums1_copy = list(nums1)
         i, j, current_index = 0, 0, 0
 
-        # if we have numbers in both the list
         while i < m and j < n:
             if nums1_copy[i] < nums2[j]:
                 nums1[current_index] = nums1_copy[i]
@@ -17,13 +35,11 @@ class Solution:
                 j += 1
                 current_index += 1
 
-        # if we have numbers left only in the nums1
         while i < m:
             nums1[current_index] = nums1_copy[i]
             i += 1
             current_index += 1
 
-        # if we have numbers left only in the nums2
         while j < n:
             nums1[current_index] = nums2[j]
             j += 1
