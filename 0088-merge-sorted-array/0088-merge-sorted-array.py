4f158d66c20c30fede69bfa2163031e8afb4f8b3
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-04-25 09:34:14 +0530

    Time: 39 ms (59.60%), Space: 16.7 MB (7.50%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,6 +4,8 @@ class Solution:
         index2 = n - 1
         merged_index = m + n - 1
 
+        # As we have to modify the same given array while doing the comparision
+        # we will start the comparision from the end, (If we start adding it from the begining, we will loose the data of nums1)
         while index1 >= 0 and index2 >= 0:
             if nums1[index1] > nums2[index2]:
                 nums1[merged_index] = nums1[index1]
@@ -12,11 +14,15 @@ class Solution:
                 nums1[merged_index] = nums2[index2]
                 index2 -= 1
             merged_index -= 1
-
+        
+        # if any element left in nums2, add it in the begining of nums1
         while index2 >= 0:
             nums1[merged_index] = nums2[index2]
             index2 -= 1
             merged_index -= 1
+            
+        # we dont need to run loop for nums1, because the remaining elements are already placed in nums1 in sorted order
+        # for example: [1,2,4,0,0,0] [3,5,6] == With the first loop => [1,2,3,4,5,6]
 
     def Tempmerge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
