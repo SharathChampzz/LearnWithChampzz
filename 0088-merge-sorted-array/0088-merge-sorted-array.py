213dd6d543c64e97df1a65bbbcc5e4af4efaaf40
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-04-25 08:58:57 +0530

    Time: 42 ms (41.42%), Space: 16.7 MB (24.76%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -6,6 +6,7 @@ class Solution:
         nums1_copy = list(nums1)
         i, j, current_index = 0, 0, 0
 
+        # if we have numbers in both the list
         while i < m and j < n:
             if nums1_copy[i] < nums2[j]:
                 nums1[current_index] = nums1_copy[i]
@@ -16,11 +17,13 @@ class Solution:
                 j += 1
                 current_index += 1
 
+        # if we have numbers left only in the nums1
         while i < m:
             nums1[current_index] = nums1_copy[i]
             i += 1
             current_index += 1
 
+        # if we have numbers left only in the nums2
         while j < n:
             nums1[current_index] = nums2[j]
             j += 1
