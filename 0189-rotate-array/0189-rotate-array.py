dfcc9478a13a8ee82a5355547edb9bfe4f44f146
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-05-23 09:39:27 +0530

    Time: 160 ms (54.42%), Space: 28 MB (68.31%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,6 +4,27 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         l = len(nums)
+        k %= l  # In case k is greater than l
+
+        # Helper function to reverse a portion of the array
+        def reverse(start: int, end: int) -> None:
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+
+        # Reverse the entire array
+        reverse(0, l - 1)
+        # Reverse the first k elements
+        reverse(0, k - 1)
+        # Reverse the rest of the array
+        reverse(k, l - 1)
+        
+    def rotateOofKComplexity(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = len(nums)
         k %= l # handle case if k > n, avoid rotating again and again
 
         new_begining = nums[-k:] # the last k number of elements would go to first after right shifting 
