43ab64800a384091e3cec720b062a190421f0858
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-06-10 08:03:58 +0530

    Time: 140 ms (15.53%), Space: 17.1 MB (9.99%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,24 +1,26 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         if not n: return True
-        
+
         total_beds = len(flowerbed)
         prev = 0
         current_plot = 0
 
         while current_plot < total_beds:
-            if not (flowerbed[current_plot] or prev):
-                # if the plan is not already planted or prev is already planted
+            already_planted = flowerbed[current_plot]
+            
+            if already_planted or prev:
+                prev = flowerbed[current_plot]
+                current_plot += 1
+                continue # if the plan is not already planted or prev is already planted
+            elif (current_plot + 1) < total_beds:
+                next = flowerbed[current_plot + 1]
+            else:
+                next = 0 # last plot and prev is empty too
 
-                if (current_plot + 1) < total_beds:
-                    next = flowerbed[current_plot + 1]
-                    if not next: # if next plot is empty, plant it
-                        n -= 1
-                        flowerbed[current_plot] = 1
-                else:
-                    if not prev:
-                        n -= 1
-                        flowerbed[current_plot] = 1
+            if not next:
+                n -= 1
+                flowerbed[current_plot] = 1
 
             prev = flowerbed[current_plot]
             current_plot += 1
