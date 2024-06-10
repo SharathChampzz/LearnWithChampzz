045e67a632e1506fcb026f2ccc5713bf47c5eb3d
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-06-10 06:25:50 +0530

    Time: 34 ms (66.83%), Space: 16.4 MB (85.51%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -7,10 +7,12 @@ class Solution:
 
         min_len = min(len1, len2)
 
+        # merge until the lowest string is merged
         while i < min_len:
             result += word1[i] + word2[i]
             i += 1
 
+        # if word1 has more characters or word2
         if len1 > min_len:
             result += word1[i:]
         else:
