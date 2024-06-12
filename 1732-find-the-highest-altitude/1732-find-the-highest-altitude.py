ba44b4acc68ace6e7ba17850ae15b567ea6e5b62
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-06-12 07:29:04 +0530

    Time: 39 ms (50.34%), Space: 16.4 MB (79.64%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -4,7 +4,7 @@ class Solution:
         prev_altitude = 0
 
         for current_altitude in gain:
-            prev_altitude = prev_altitude + current_altitude
+            prev_altitude = prev_altitude + current_altitude # this will be like new altitude, the bike is at
             max_altitude = max(max_altitude, prev_altitude)
 
         return max_altitude
