1e5f49af4f3b2dcc275b347bd99d3c7664d3bb36
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-06-22 06:32:54 +0530

    Time: 188 ms (37.42%), Space: 16.9 MB (99.75%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -3,7 +3,11 @@ class Solution:
         # logic used: lets try to revisit non-visited/disconnected nodes again and again
         # the number of time, we revisit or try on disconnected nodes. We will get the province counts.
         
+        n = len(isConnected)
+        all_nodes = {i for i in range(n)}
         visited_node = set()
+        
+        province_count = 0
 
         # bfs using adjacent matrix
         def bfs(new_node):
@@ -13,18 +17,14 @@ class Solution:
             while queue:
                 node = queue.popleft()
 
-                for index, neighbour in enumerate(isConnected[node]):
-                    if index in visited_node or neighbour == 0:
+                for index in range(n):
+                    if index in visited_node or isConnected[node][index] == 0:
                         continue
 
                     queue.append(index)
                     visited_node.add(index)
                     all_nodes.remove(index)
 
-        n = len(isConnected)
-        all_nodes = {i for i in range(n)}
-
-        province_count = 0
         while all_nodes:
             start_node = all_nodes.pop()
             bfs(start_node)
