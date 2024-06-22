f20d8c950035b01f6ac3215d07f2a193f311ba68
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-06-22 06:37:43 +0530

    Time: 180 ms (74.44%), Space: 17 MB (98.46%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,33 +1,23 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        # logic used: lets try to revisit non-visited/disconnected nodes again and again
-        # the number of time, we revisit or try on disconnected nodes. We will get the province counts.
-        
-        n = len(isConnected)
-        all_nodes = {i for i in range(n)}
-        visited_node = set()
-        
-        province_count = 0
-
-        # bfs using adjacent matrix
-        def bfs(new_node):
-            queue = deque([new_node])
-            visited_node.add(new_node)
+        def bfs(start_node: int):
+            queue = deque([start_node])
+            visited[start_node] = True
 
             while queue:
                 node = queue.popleft()
+                for neighbor in range(n):
+                    if not visited[neighbor] and isConnected[node][neighbor] == 1:
+                        queue.append(neighbor)
+                        visited[neighbor] = True
 
-                for index in range(n):
-                    if index in visited_node or isConnected[node][index] == 0:
-                        continue
+        n = len(isConnected)
+        visited = [False] * n
+        province_count = 0
 
-                    queue.append(index)
-                    visited_node.add(index)
-                    all_nodes.remove(index)
-
-        while all_nodes:
-            start_node = all_nodes.pop()
-            bfs(start_node)
-            province_count += 1
+        for i in range(n):
+            if not visited[i]:
+                bfs(i)
+                province_count += 1
 
         return province_count
