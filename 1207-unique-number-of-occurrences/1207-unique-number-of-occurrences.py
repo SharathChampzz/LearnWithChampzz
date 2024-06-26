d2b820cee83c690760cb1707b6ddd2bfbd11b285
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-06-26 06:24:01 +0530

    Time: 44 ms (38.22%), Space: 16.6 MB (90.29%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,10 +1,5 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        # Method 1
-        count = Counter(arr) # from collections import Counter, It will do the count
-        return len(count) == len(set(count.values()))
-        
-        # Method 2
         occurences = defaultdict(int)
         unique_counts = set()
 
@@ -14,3 +9,8 @@ class Solution:
             # print(f'Occurences: {occurences}, UniqueCounts: {unique_counts}')
 
         return len(occurences) == len(set(occurences.values())) # remove the duplicates from count values, and see if the length still remain same.
+        
+        
+        # # simplified code
+        # count = Counter(arr) # from collections import Counter, It will do the count
+        # return len(count) == len(set(count.values()))
