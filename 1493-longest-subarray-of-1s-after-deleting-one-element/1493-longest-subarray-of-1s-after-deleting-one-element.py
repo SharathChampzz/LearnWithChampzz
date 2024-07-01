55fdce4cfb55d7a17b4e6498c03a572e9f186bab
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-07-01 07:02:24 +0530

    Time: 461 ms (48.57%), Space: 20 MB (28.15%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        # same question as previous, just that k == 1 in this condition and we need to subtract 1 from the result, as we are deleting it.
+        right = 0
+        length = len(nums)
+        max_size = 0
+        k = 1
+
+        for left in range(length):
+
+            while right < length and (nums[right] or k):
+                if nums[right] == 0:
+                    k -= 1
+                right += 1
+
+            max_size = max(max_size, right-left)
+            # print(f'Max Size: {max_size}, left:{left} and right:{right}')
+
+            if nums[left] == 0:
+                k += 1 # the first number was 0 and we must have swapped and decremented k value
+
+            if right >= length:
+                break
+
+        return max_size-1
