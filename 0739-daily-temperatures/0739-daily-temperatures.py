f8f501e15518e7dc086225c11c47904334d27061
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-07-05 07:16:38 +0530

    Time: 893 ms (52.24%), Space: 31.6 MB (30.01%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,22 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # we are asked to get next greater element, So we will use monotonic decreasing stack. 
+        # Remember, This the not the next greater element (When we sort) Instead a number which is greater when we iterate from left to right
+        # Whenever you encounter big number, we will tell the lower numbers that the current number is bigger than you, and update the answer
+        # if we are not finding bigger number, we will add them to the waiting list (stack)
+        n = len(temperatures)
+        result = [0] * n
+        stack = []
+
+        for index, i in enumerate(temperatures):
+            if not stack:
+                stack.append((index, i))
+                continue
+
+            while stack and stack[-1][1] < i:
+                old_index, old_value = stack.pop()
+                result[old_index] = index - old_index
+
+            stack.append((index, i))
+
+        return result
