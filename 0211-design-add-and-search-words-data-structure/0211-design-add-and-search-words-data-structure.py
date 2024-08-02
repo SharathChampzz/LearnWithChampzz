3d402034929db4de859a8a8e98bc3b2d06f9bdec
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-08-02 14:58:30 +0530

    Time: 1357 ms (85.94%), Space: 64.1 MB (66.30%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -18,7 +18,22 @@ class WordDictionary:
 
         curr.end = True
 
-    def search(self, word: str) -> bool:
+    def search_via_dfs(self, word, index, node) -> bool:
+        if index == len(word):
+            return node.end
+
+        char = word[index]
+
+        if char == '.':
+            for dotnode in node.childs.values():
+                if self.search_via_dfs(word, index + 1, dotnode):
+                    return True
+        else:
+            if char not in node.childs:
+                return False
+            return self.search_via_dfs(word, index + 1, node.childs[char])
+
+    def search_via_bfs(self, word: str) -> bool:
         nodes = [self.root] # as we need to support dot, we will need to track multiple trienodes
 
         for char in word:
@@ -35,6 +50,10 @@ class WordDictionary:
                 return False
 
         return any(node.end for node in nodes)
+
+    def search(self, word: str) -> bool:
+        return self.search_via_dfs(word, 0, self.root)
+        # return self.search_via_bfs(word)
 
 
 # Your WordDictionary object will be instantiated and called as such:
