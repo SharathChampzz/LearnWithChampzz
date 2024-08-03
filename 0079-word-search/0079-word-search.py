f3c4849f6c2473eaaf4ea7511b1601e2b6a885f5
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-08-03 08:52:24 +0530

    Time: 3107 ms (86.86%), Space: 16.6 MB (51.45%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -4,29 +4,28 @@ class Solution:
         n = len(board[0])
         result_len = len(word)
 
-        def search(i, j, index, visited):
+        def search(i, j, index):
             if index >= result_len:
                 return True
             
-            if i < 0 or j < 0 or i >= m or j >= n or (i, j) in visited or board[i][j] != word[index]:
+            if i < 0 or j < 0 or i >= m or j >= n or board[i][j] != word[index]:
                 return False
 
-            index += 1
-            visited.append((i, j))
+            temp = board[i][j]
+            board[i][j] = "#"
 
-            found = search(i + 1, j + 0, index, visited) or \
-                    search(i + 0, j + 1, index, visited) or \
-                    search(i - 1, j + 0, index, visited) or \
-                    search(i + 0, j - 1, index, visited)
+            found = search(i + 1, j + 0, index + 1) or \
+                    search(i + 0, j + 1, index + 1) or \
+                    search(i - 1, j + 0, index + 1) or \
+                    search(i + 0, j - 1, index + 1)
+
+            board[i][j] = temp
             
-            index -= 1
-            visited.remove((i, j))
-
             return found
 
         for i in range(m):
             for j in range(n):
-                if board[i][j] == word[0] and search(i, j, 0, []):
+                if board[i][j] == word[0] and search(i, j, 0):
                     return True
 
         return False
