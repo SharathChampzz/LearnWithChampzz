4c7b1bb41eced4d783ba02f05237e07f4e55e8a9
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-08-11 16:49:42 +0530

    Time: 1581 ms (69.42%), Space: 16.9 MB (30.32%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        total_nums = len(nums)
+
+        # initialize the DB array
+        longest = [1] * total_nums # atleast 1 will be the length for any index
+
+        # from the end fill up the DB array
+        for i in range(total_nums - 1, -1, -1):
+            # read all the next available greater nums and its longest values
+            next_max = 0
+            for j in range(i+1, total_nums):
+                if nums[j] > nums[i]:
+                    next_max = max(next_max, longest[j])
+
+            longest[i] += next_max  # nothing but 1 + max of next greatest length
+
+        return max(longest)
+
+
