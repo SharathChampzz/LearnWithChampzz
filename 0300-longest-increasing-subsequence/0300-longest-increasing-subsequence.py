e695cf84d8a65b2513b2ed03fb578df4432d8453
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-08-11 16:51:15 +0530

    Time: 1585 ms (69.30%), Space: 16.8 MB (65.05%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -6,7 +6,8 @@ class Solution:
         longest = [1] * total_nums # atleast 1 will be the length for any index
 
         # from the end fill up the DB array
-        for i in range(total_nums - 1, -1, -1):
+        # total_nums - 2 should be fine, for total_nums - 1, j loop will anyways not run
+        for i in range(total_nums - 2, -1, -1):
             # read all the next available greater nums and its longest values
             next_max = 0
             for j in range(i+1, total_nums):
