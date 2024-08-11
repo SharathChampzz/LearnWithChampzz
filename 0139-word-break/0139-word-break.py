c43cff598c4711f89efcd32a31150ee72306687a
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-08-11 09:00:34 +0530

    Time: 43 ms (38.76%), Space: 16.5 MB (78.40%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,5 +1,56 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        str_len = len(s)
+
+        status = [False] * (str_len + 1)
+        status[-1] = True # we are exactly at end
+
+        for i in range(str_len-1, -1, -1):
+            temp = []
+
+            for word in wordDict:
+                substr_len = len(word)
+
+                if (i + substr_len) > str_len:
+                    continue # it will overflow
+
+                if s[i: i+substr_len] == word:
+                    temp.append(status[i+substr_len]) # we can stop here actually if any one is found
+            
+            if any(temp):
+                status[i] = True
+
+        return status[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def InternetwordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
         dp = [False] * (n + 1)
         dp[0] = True
