0729efa28f6b6995767671999952737593e7cf63
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-08-12 21:42:10 +0530

    Time: 39 ms (85.42%), Space: 16.5 MB (76.13%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,25 +1,24 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        # Take sum from left and top, to get the total paths for the current index
-        
         m, n = len(obstacleGrid), len(obstacleGrid[0])
-
         if obstacleGrid[0][0] == 1:
             return 0 # entry itself blocked
 
-        # iterate and mark blockages as 'X'
+        obstacleGrid[0][0] = 1
+
+        # for i in range(m):
+        #     for j in range(n):
+        #         if obstacleGrid[i][j] == 1:
+        #             obstacleGrid[i][j] = 'X'
+
+        # if 
+
         for i in range(m):
             for j in range(n):
                 if obstacleGrid[i][j] == 1:
+                    if i == 0 and j == 0:
+                        continue
                     obstacleGrid[i][j] = 'X'
-
-        # base case
-        obstacleGrid[0][0] = 1
-
-        for i in range(m):
-            for j in range(n):
-                if i == 0 and j == 0:
-                    continue # skip this is already set
 
                 sum = 0
                 # [Top, Left]
@@ -28,5 +27,7 @@ class Solution:
                         sum += obstacleGrid[x][y]
 
                 obstacleGrid[i][j] = sum
+
+        # print(obstacleGrid)
 
         return obstacleGrid[m-1][n-1]
