a2730bc4dcb2fa6ee7a1b4d41c95c99e7654b8af
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-08-12 21:52:44 +0530

    Time: 48 ms (29.44%), Space: 16.5 MB (96.54%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,33 +1,28 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         m, n = len(obstacleGrid), len(obstacleGrid[0])
-        if obstacleGrid[0][0] == 1:
-            return 0 # entry itself blocked
+                
+        if obstacleGrid[0][0] == 1 or obstacleGrid[m-1][n-1]:
+            return 0 # entry or exit are blocked
 
+        # set base case
         obstacleGrid[0][0] = 1
-
-        # for i in range(m):
-        #     for j in range(n):
-        #         if obstacleGrid[i][j] == 1:
-        #             obstacleGrid[i][j] = 'X'
-
-        # if 
-
+        
+        # iterate through the grid and take sum from top and left
         for i in range(m):
             for j in range(n):
+                
+                if i == 0 and j == 0:
+                    continue # ignore base index
+                    
                 if obstacleGrid[i][j] == 1:
-                    if i == 0 and j == 0:
-                        continue
-                    obstacleGrid[i][j] = 'X'
+                    obstacleGrid[i][j] = 'X' # mark it as blocked
+                    continue
 
-                sum = 0
-                # [Top, Left]
-                for x, y in [(i-1,j), (i,j-1)]:
-                    if x >= 0 and y >= 0 and obstacleGrid[i][j] != 'X':
-                        sum += obstacleGrid[x][y]
-
-                obstacleGrid[i][j] = sum
-
-        # print(obstacleGrid)
+                cords = [(i-1,j), (i,j-1)] # [Top, Left]
+                
+                for x, y in cords:
+                    if x >= 0 and y >= 0 and obstacleGrid[x][y] != 'X':
+                        obstacleGrid[i][j] += obstacleGrid[x][y]
 
         return obstacleGrid[m-1][n-1]
