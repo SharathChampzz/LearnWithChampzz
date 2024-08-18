afe42a5a690d2afff4d8bb430b68e029765807a9
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-08-18 19:49:40 +0530

    Time: 45 ms (88.44%), Space: 16.5 MB (90.45%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        counter = Counter(magazine)
+
+        for char in ransomNote:
+            found = counter.get(char)
+
+            if not found:
+                return False
+
+            counter[char] -= 1
+
+        return True
